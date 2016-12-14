15917a594d6b57a74665fa4d08ffa708b59d2204
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-12-14 17:05:12 +0100

    api youtube

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 // Create a new component. This component should produce some HTML
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyB5DiecHcm6y8Nz5wTaksz2J9PZ2S6KKOI";
 
-const App = () => {
-	return <div><SearchBar /></div>;
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+		this.setState({ videos: data });
+	});
+
+}
+
+	render() {
+		return (
+			<div><SearchBar /></div>
+		);
+	}	
 }
 
 
