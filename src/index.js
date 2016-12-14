8e8e418ca9f6b0ac274e93809a19cb7b0b60a7a0
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-12-14 14:56:46 +0100

    input DONE

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = "AIzaSyB5DiecHcm6y8Nz5wTaksz2J9PZ2S6KKOI";
 
 const App = () => {
-	return <div>Hi there</div>;
+	return <div><SearchBar /></div>;
 }
 
 
