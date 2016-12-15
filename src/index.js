7a754fc68ce7ef3879e73ea59c28f91c6383fcd0
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-12-15 10:53:19 +0100

    .debounce

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -34,6 +35,10 @@ videoSearch(term) {
 }
 
 	render() {
+
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+
 		return (
 			<div>
 				<SearchBar onSearchTermChange ={term => this.videoSearch(term)}/>
